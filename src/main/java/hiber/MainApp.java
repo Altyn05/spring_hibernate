a8992056d3aa7232516cfc20442a7bc4adfea4ab
Altyn05 <Altynkus1989@gmail.com>
2021-11-03 02:28:31 +0600

    Сделал

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,50 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+      }
+
+
+
+      Car mazda = new Car("mazda",626);
+      Car mersedes = new Car("mersedes",212);
+      Car BMW = new Car("BMW",7);
+      User user5 = new User("User5","Lastname5","user5@mail.ru");
+      User user6 = new User("User6","Lastname6","user6@mail.ru");
+      User user7 = new User("User7","Lastname7","user7@mail.ru");
+      user5.setCar(mazda);
+      user6.setCar(mersedes);
+      user7.setCar(BMW);
+      userService.add(user5);
+      userService.add(user6);
+      userService.add(user7);
+      System.out.println(userService.getUserByCar("mazda",626).toString());
+      System.out.println(userService.getUserByCar("mersedes",212).toString());
+      System.out.println(userService.getUserByCar("BMW",7).toString());
+      context.close();
+   }
+}
